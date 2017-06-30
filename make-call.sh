9a2e948682cee8b288f7f003696203fdcfdd79d6
reverendus <reverendus@invalid>
2017-06-30 18:30:10 +0200

    `make-call.sh` updated to build the contract every time before the DSProxy call

--- make-call.sh
+++ make-call.sh
@@ -3,5 +3,7 @@
 [ -z ${DS_PROXY} ] && echo "Please set DS_PROXY" && exit -1
 [ -z ${ETH_FROM} ] && echo "Please set ETH_FROM" && exit -1
 
+dapp clean
+dapp build
 seth send $DS_PROXY "execute(bytes,bytes)(bytes32)" $(cat out/ProxyCall.bin) $(seth calldata 'call()')
 
