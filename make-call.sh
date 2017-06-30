57f0c9d0811a36b91f110c13cbd87034a839e4ca
reverendus <reverendus@invalid>
2017-06-30 16:54:16 +0200

    Added a shell script to call DSProxy with this sample contract

---
+++ make-call.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+[ -z ${DS_PROXY} ] && echo "Please set DS_PROXY" && exit -1
+[ -z ${ETH_FROM} ] && echo "Please set ETH_FROM" && exit -1
+
+seth send $DS_PROXY "execute(bytes,bytes)(bytes32)" $(cat out/ProxyCall.bin) $(seth calldata 'call()')
+
