9185074b884cf443ac76c3c8ac5ca030ea93b15e
reverendus <reverendus@invalid>
2017-07-03 11:49:01 +0200

    Ethereum transaction manager (work in progress)
    Managed to call two different contracts in one transaction,
    transfering necessary ERC20 token balances beforehand
    and returning them to the initiator after the execution.

--- src/call.sol
+++ src/call.sol
@@ -2,30 +2,6 @@ pragma solidity ^0.4.11;
 
 import "erc20/erc20.sol";
 
-contract ProxyCall { 
-	function call()
-	{
-        address token = 0x224c2202792B11c5ac5bAaAA8284e6edb60f7174;
-        address source = 0x002ca7F9b416B2304cDd20c26882d1EF5c53F611;
-        address target = 0x00348E4084567Ce2E962B64ABe5A54BaB256Bc26;
-
-        //ERC20(token).transferFrom(source, target, 10 wei);
-        ERC20(token).transferFrom(msg.sender, target, 10 wei);
-        //ERC20(token).transfer(target, 10 wei);
-
-        //ERC20(token).transferFrom(target, source, 4 wei);
-        assembly {
-            // 0x23b872dd00000000000000000000000000348e4084567ce2e962b64abe5a54bab256bc26000000000000000000000000002ca7f9b416b2304cdd20c26882d1ef5c53f6110000000000000000000000000000000000000000000000000000000000000004
-            mstore(0x2000, 0x23b872dd00000000000000000000000000348e4084567ce2e962b64abe5a54ba)
-            mstore(0x2020, 0xb256bc26000000000000000000000000002ca7f9b416b2304cdd20c26882d1ef)
-            mstore(0x2040, 0x5c53f61100000000000000000000000000000000000000000000000000000000)
-            mstore(0x2060, 0x0000000400000000000000000000000000000000000000000000000000000000)
-            let succeeded := call(sub(gas, 5000), token, 0, 0x2000, 0x64, 0, 0)
-            jumpi(invalidJumpLabel, iszero(succeeded))
-        }
-	}
-}
-
 contract TransactionManager {
 
     function execute(bytes balances, bytes steps) {
@@ -33,6 +9,7 @@ contract TransactionManager {
         for (uint i = 0; i < balances.length/52; i++) {
             address token = getAddressAt(balances, 0x34*i);
             uint256 value = getWordAt(balances, 0x34*i + 0x14);
+
             ERC20(token).transferFrom(msg.sender, this, value);
         }
 
@@ -40,14 +17,24 @@ contract TransactionManager {
         uint256 stepLocation = 0;
         while (stepLocation < steps.length) {
             uint256 stepLength = getWordAt(steps, stepLocation);
-            //TODO
-        }
-        // TODO
+            address stepAddress = getAddressAt(steps, stepLocation + 0x20);
 
-        // transfer remaining balances back to the caller
-        for (uint i2 = 0; i2 < balances.length/52; i2++) {
-            address token2 = getAddressAt(balances, 0x34*i2);
-            ERC20(token2).transfer(msg.sender, ERC20(token2).balanceOf(this));
+            assembly {
+                let succeeded := call(sub(gas, 5000), stepAddress, 0, add(add(add(steps, 0x20), stepLocation), 0x34), sub(stepLength, 0x14), 0, 0)
+                jumpi(invalidJumpLabel, iszero(succeeded))
+            }
+            stepLocation = stepLocation + 0x20 + stepLength;
+        }
+
+        returnBalances(balances);
+    }
+
+    // transfer remaining balances back to the caller
+    function returnBalances(bytes balances) internal {
+        for (uint i = 0; i < balances.length/52; i++) {
+            address token = getAddressAt(balances, 0x34*i);
+
+            ERC20(token).transfer(msg.sender, ERC20(token).balanceOf(this));
         }
     }
 
@@ -62,6 +49,19 @@ contract TransactionManager {
         assembly {
             result := div(and(word, 0xffffffffffffffffffffffffffffffffffffffff000000000000000000000000), 0x1000000000000000000000000)
         }
+    }
+
+    function getBytesAt(bytes array, uint256 location, uint256 length) internal returns (bytes memory result) {
+        result = new bytes(length);
+        //TODO
+    }
+}
+
+contract TestContract {
+    uint256 public counter;
+
+    function inc(uint256 value) {
+        counter += value;
     }
 }
 
