23bd93a897e32ebdccf422aa16a549878a989a5e
reverendus <reverendus@invalid>
2017-07-03 12:32:46 +0200

    Method rename

--- src/call.sol
+++ src/call.sol
@@ -14,8 +14,8 @@ contract TransactionManager {
     // pulls requested amount of each token from the sender
     function pullBalances(bytes balancesData) internal {
         for (uint index = 0; index < balancesData.length/0x34; index++) {
-            address token = getAddressAt(balancesData, 0x34*index);
-            uint256 value = getWordAt(balancesData, 0x34*index + 0x14);
+            address token = addressAt(balancesData, 0x34*index);
+            uint256 value = wordAt(balancesData, 0x34*index + 0x14);
 
             ERC20(token).transferFrom(msg.sender, this, value);
         }
@@ -24,7 +24,7 @@ contract TransactionManager {
     // returns remaining balances of each token to the sender
     function returnBalances(bytes balancesData) internal {
         for (uint index = 0; index < balancesData.length/0x34; index++) {
-            address token = getAddressAt(balancesData, 0x34*index);
+            address token = addressAt(balancesData, 0x34*index);
 
             ERC20(token).transfer(msg.sender, ERC20(token).balanceOf(this));
         }
@@ -34,8 +34,8 @@ contract TransactionManager {
         // execute steps
         uint256 stepLocation = 0;
         while (stepLocation < stepsData.length) {
-            uint256 stepLength = getWordAt(stepsData, stepLocation);
-            address stepAddress = getAddressAt(stepsData, stepLocation + 0x20);
+            uint256 stepLength = wordAt(stepsData, stepLocation);
+            address stepAddress = addressAt(stepsData, stepLocation + 0x20);
 
             assembly {
                 let succeeded := call(sub(gas, 5000), stepAddress, 0, add(add(add(stepsData, 0x20), stepLocation), 0x34), sub(stepLength, 0x14), 0, 0)
@@ -45,14 +45,14 @@ contract TransactionManager {
         }
     }
 
-    function getWordAt(bytes array, uint256 location) internal returns (uint256 result) {
+    function wordAt(bytes array, uint256 location) internal returns (uint256 result) {
         assembly {
             result := mload(add(array, add(0x20, location)))
         }
     }
 
-    function getAddressAt(bytes array, uint256 location) internal returns (address result) {
-        uint256 word = getWordAt(array, location);
+    function addressAt(bytes array, uint256 location) internal returns (address result) {
+        uint256 word = wordAt(array, location);
         assembly {
             result := div(and(word, 0xffffffffffffffffffffffffffffffffffffffff000000000000000000000000), 0x1000000000000000000000000)
         }
