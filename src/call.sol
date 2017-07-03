ea210cb564b458ae527d9dcd377f78d8aefee4f4
reverendus <reverendus@invalid>
2017-07-03 12:38:53 +0200

    `wordAt` -> `uint256At'

--- src/call.sol
+++ src/call.sol
@@ -15,8 +15,7 @@ contract TransactionManager {
     function pullBalances(bytes balancesData) internal {
         for (uint index = 0; index < balancesData.length/0x34; index++) {
             address token = addressAt(balancesData, 0x34*index);
-            uint256 value = wordAt(balancesData, 0x34*index + 0x14);
-
+            uint256 value = uint256At(balancesData, 0x34*index + 0x14);
             ERC20(token).transferFrom(msg.sender, this, value);
         }
     }
@@ -25,7 +24,6 @@ contract TransactionManager {
     function returnBalances(bytes balancesData) internal {
         for (uint index = 0; index < balancesData.length/0x34; index++) {
             address token = addressAt(balancesData, 0x34*index);
-
             ERC20(token).transfer(msg.sender, ERC20(token).balanceOf(this));
         }
     }
@@ -34,7 +32,7 @@ contract TransactionManager {
         // execute steps
         uint256 stepLocation = 0;
         while (stepLocation < stepsData.length) {
-            uint256 stepLength = wordAt(stepsData, stepLocation);
+            uint256 stepLength = uint256At(stepsData, stepLocation);
             address stepAddress = addressAt(stepsData, stepLocation + 0x20);
 
             assembly {
@@ -45,14 +43,14 @@ contract TransactionManager {
         }
     }
 
-    function wordAt(bytes array, uint256 location) internal returns (uint256 result) {
+    function uint256At(bytes array, uint256 location) internal returns (uint256 result) {
         assembly {
             result := mload(add(array, add(0x20, location)))
         }
     }
 
     function addressAt(bytes array, uint256 location) internal returns (address result) {
-        uint256 word = wordAt(array, location);
+        uint256 word = uint256At(array, location);
         assembly {
             result := div(and(word, 0xffffffffffffffffffffffffffffffffffffffff000000000000000000000000), 0x1000000000000000000000000)
         }
