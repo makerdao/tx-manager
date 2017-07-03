c9df1ce4e5bfc745bd57abd583a84ea6ebab4bd4
reverendus <reverendus@invalid>
2017-07-03 15:12:35 +0200

    Contract code simplified

--- src/call.sol
+++ src/call.sol
@@ -6,42 +6,30 @@ import "erc20/erc20.sol";
 
 contract TransactionManager is DSAuth, DSNote {
 
-    function execute(address[] tokens, bytes invocationsData) {
-        pullBalances(tokens);
-        invokeContracts(invocationsData);
-        returnBalances(tokens);
+    function execute(address[] tokens, bytes script) {
+        // pull the entire amount of each token from the sender
+        for (uint i = 0; i < tokens.length; i++)
+            ERC20(tokens[i]).transferFrom(msg.sender, this, ERC20(tokens[i]).balanceOf(msg.sender));
+
+        // sequentially call contacts, abort on failed calls
+        invokeContracts(script);
+
+        // returns remaining balances of each token to the sender
+        for (uint j = 0; j < tokens.length; j++)
+            ERC20(tokens[j]).transfer(msg.sender, ERC20(tokens[j]).balanceOf(this));
     }
 
-    // pulls requested amount of each token from the sender
-    function pullBalances(address[] tokens) internal {
-        for (uint i = 0; i < tokens.length; i++) {
-            address token = tokens[i];
-            uint256 value = ERC20(token).balanceOf(msg.sender);
-            ERC20(token).transferFrom(msg.sender, this, value);
-        }
-    }
-
-    // returns remaining balances of each token to the sender
-    function returnBalances(address[] tokens) internal {
-        for (uint i = 0; i < tokens.length; i++) {
-            address token = tokens[i];
-            ERC20(token).transfer(msg.sender, ERC20(token).balanceOf(this));
-        }
-    }
-
-    // sequentially call contacts, abort on failed calls
-    function invokeContracts(bytes invocationsData) internal {
-        // execute steps
+    function invokeContracts(bytes script) internal {
         uint256 location = 0;
-        while (location < invocationsData.length) {
-            uint256 length = uint256At(invocationsData, location);
+        while (location < script.length) {
+            uint256 length = uint256At(script, location);
 
-            address contractAddress = addressAt(invocationsData, location + 0x20);
-            uint256 calldataStart = locationOf(invocationsData, location + 0x20 + 0x14);
+            address contractAddress = addressAt(script, location + 0x20);
+            uint256 calldataStart = locationOf(script, location + 0x20 + 0x14);
             uint256 calldataLength = length - 0x14;
             assembly {
-                let succeeded := call(sub(gas, 5000), contractAddress, 0, calldataStart, calldataLength, 0, 0)
-                jumpi(invalidJumpLabel, iszero(succeeded))
+                let ok := call(sub(gas, 5000), contractAddress, 0, calldataStart, calldataLength, 0, 0)
+                jumpi(invalidJumpLabel, iszero(ok))
             }
 
             location += (0x20 + length);
@@ -57,7 +45,8 @@ contract TransactionManager is DSAuth, DSNote {
     function addressAt(bytes data, uint256 location) internal returns (address result) {
         uint256 word = uint256At(data, location);
         assembly {
-            result := div(and(word, 0xffffffffffffffffffffffffffffffffffffffff000000000000000000000000), 0x1000000000000000000000000)
+            result := div(and(word, 0xffffffffffffffffffffffffffffffffffffffff000000000000000000000000),
+                          0x1000000000000000000000000)
         }
     }
 
